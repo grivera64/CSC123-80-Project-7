b4089341ae761a835f1122c0b1099531a0e0b964
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-14 23:31:45 -0700

    Feat(ThrowTest1): Add divide method to Divider1

---
+++ ThrowTest1/Divider1.java
@@ -0,0 +1,23 @@
+package project7.ThrowTest1;
+
+/*
+ * 
+ * Name: Divider1.java
+ * Purpose: Stores a method for dividing
+ * Author: grivera64
+ * Date: 04/14/2021
+ * 
+ */
+
+public class Divider1
+{
+	
+	/* Returns the quotient of a and b */
+	public int divideA(int a, int b)
+	{
+		
+		return a / b;
+		
+	}
+	
+}
